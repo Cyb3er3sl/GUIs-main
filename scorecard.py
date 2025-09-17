226dadad4e5b62d7afbb624127d2ba95d337cc06
584463 <584463@eriesd.org>
2025-09-17 12:51:59 -0400

    Done with the test.

--- scorecard.py
+++ scorecard.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Sequence
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from die import Die
@@ -8,34 +8,41 @@ if TYPE_CHECKING:
 class Scorecard:
     def __init__(self):
         self.score = 0
+        self.used_categories = set()
         self.ones = None
+        self.full_house = None
 
-    def score_ones(self, dice: list[Die]) -> int:
-        round_score = 0
-        dice_vals = [die.value for die in dice]
-        for val in dice_vals:
-            if val == 1:
-                round_score += 1
-        #print(round_score)
+    def _get_values(self, dice):
+        """Helper: normalize to list of ints"""
+        return [die.value if hasattr(die, "value") else die for die in dice]
+
+    def score_ones(self, dice) -> int | str:
+        if "ones" in self.used_categories:
+            return "Ones already used"
+
+        dice_vals = self._get_values(dice)
+        round_score = sum(val for val in dice_vals if val == 1)
+
         self.score += round_score
         self.ones = round_score
+        self.used_categories.add("ones")
         return round_score
-    
 
-    def score_full_house(self, dice: list[Die]):
-        round_score = 0
-        values =  [die.value for die in dice]
+    def score_full_house(self, dice) -> int | str:
+        if "full_house" in self.used_categories:
+            return "Full house already used"
+
+        values = self._get_values(dice)
         values.sort()
-        
-        print(values)
-        # For 3, 3, 5, 5, 5
-        #         3   ==   3                5      ==     5             5       ==   5
-        if (values[0] == values[1]) and (values[2] == values[3]) and (values[3] == values[4]):
+
+        if (values[0] == values[1] and values[2] == values[3] and values[3] == values[4]):
             round_score = 25
-            
-        elif (values[0] == values[1]) and (values[1] == values[2]) and (values[3] == values[4]):
+        elif (values[0] == values[1] and values[1] == values[2] and values[3] == values[4]):
             round_score = 25
-        else: 
+        else:
             round_score = 0
 
-        print(round_score)
+        self.score += round_score
+        self.full_house = round_score
+        self.used_categories.add("full_house")
+        return round_score
--- test_scorecard.py
+++ test_scorecard.py
@@ -1,23 +1,22 @@
 from scorecard import Scorecard
- 
- 
+
 def test_score_ones():
     testing_sc = Scorecard()
     score = testing_sc.score_ones([1,1,2,3,4])
     assert score == 2
- 
+
     testing_sc = Scorecard()
     score = testing_sc.score_ones([1,1,2,1,1])
     assert score == 4
- 
+
     testing_sc = Scorecard()
     score = testing_sc.score_ones([5,5,5,5,5])
     assert score == 0
- 
+
 def test_try_to_score_after_scoring():
     testing_sc = Scorecard()
     score = testing_sc.score_ones([1,1,2,3,4])
     assert score == 2
- 
+
     score = testing_sc.score_ones([1,1,2,1,1])
     assert score == "Ones already used"
